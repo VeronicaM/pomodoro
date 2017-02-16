f9eda2f97a591aaacd71ea7436ba60fca4fc9066
Veronica Mihai <Veronica Mihai>
2017-02-16 03:21:32 +0200

    added reset button and take break functionality

--- public/js/app.js
+++ public/js/app.js
@@ -2,7 +2,12 @@ require("../css/app.scss");
 const favIco =require("../favicon.png");
 $(function () 
  { 
- 	let countdown = 25, settings={seconds:59,minutes:countdown}, minutes= settings.minutes, seconds=settings.seconds;
+ 	let countdown = 2, 
+ 		settings={seconds:5,minutes:countdown,break:1}, 
+ 		minutes= settings.minutes, 
+ 		seconds=settings.seconds,
+ 		pomodoroId="",
+ 		isBreak=false;
  $(document).ready(function(){
        const link = $("<link></link>");
 	    link[0].type = 'image/x-icon';
@@ -12,9 +17,37 @@ $(function ()
        setTimeMinutes(countdown);
       
 	   $("#start").on("click",function(e){
- 			 elapseTime();
+	   	 const toggle = $(this).text();
+	   		if(toggle=="Start"){
+	   			$(this).text("Stop");
+	   			resetPomodoro();
+ 			    elapseTime();	
+	   		}else if(toggle == "Stop"){
+	   			if(confirm("Are you sure you want to stop the timer ?")){
+	   				$(this).text("Start");
+	   				resetPomodoro();
+	   			}
+	   		}
+	   		else if(toggle == "Take Break"){
+	   			resetPomodoro();
+	   			$(this).text("Start");
+	   			$(".message").text("Break Time");
+	   			minutes=settings.break;
+ 			    elapseTime();
+ 			    isBreak = true;
+	   		}
+	   		
 	   });
  });
+     function resetPomodoro(){
+     	minutes = settings.minutes;
+     	seconds = settings.seconds;
+     	 $(".message").text("Time until break"); 
+     	 $("title").text("Pomodoro");
+     	clearInterval(pomodoroId);
+     	setTimeMinutes(minutes);
+	    setTimeSeconds(0);
+     }
 	 function setTimeMinutes(timeV){
 	 	const minutes = timeV.toString().length >1 ? timeV.toString().split(""): ("0"+timeV.toString()).split("");
 	 	$("time div:nth-of-type(1) span").text(minutes[0]);
@@ -27,10 +60,10 @@ $(function ()
 	 	$("time div:nth-of-type(5) span").text(seconds[1]);
 	 }
 	 function elapseTime(){
-               minutes--;
+                minutes--;
 			    setTimeMinutes(minutes);
 			    setTimeSeconds(seconds);
-			    const countdownId = setInterval(function(){
+			    pomodoroId = setInterval(function(){
 			      //$("time div:nth-of-type(2) span").removeClass("runAnimation");	
 		       	  if(seconds==0){
 		       	  	if(minutes!=0){
@@ -39,15 +72,25 @@ $(function ()
 		       	  	   seconds=settings.seconds;
 		       	  	//  	animateDigit();
 		       	  	}else{
-		       	  		console.log("finished countdown",seconds,minutes);
-		       	  		clearInterval(countdownId);
+		       	  		//console.log("finished countdown",seconds,minutes);
+		       	  		clearInterval(pomodoroId);
+		       	  		
+		       	  		if(!isBreak){
+		       	  		  $("#start").text("Take Break");		
+		       	  		}
+		       	  		else{
+		       	  			resetPomodoro();
+		       	  			isBreak= false;	
+		       	  		} 
+		       	  		
 		       	  	}
 		       	  }else{
 		       	  		seconds--;
 	       	        	setTimeSeconds(seconds);
+	       	        	 updateTitlePomodoro();
 		       	  }
 		       	
-	       	     updateTitlePomodoro();
+	       	    
 	       	  	
 		    },1000);	
 	 }
