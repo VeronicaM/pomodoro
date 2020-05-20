bdaec59f46293f8baf2e5da1305d47cb2f107a8e
Veronica MIHAI <mihaiveronica@ymail.com>
2020-05-20 08:02:40 +0300

    fix lint:format

--- lambdas/getForismaticQuote/getQuote.js
+++ lambdas/getForismaticQuote/getQuote.js
@@ -1,52 +1,57 @@
-const https = require('https');
-exports.handler = async (event) => {
-    let dataString = '';
+const https = require("https");
 
-    const response = await new Promise((resolve, reject) => {
-        const req = https.get("https://api.forismatic.com/api/1.0/?method=getQuote&format=text&lang=en", function (res) {
-            res.on('data', chunk => {
-                dataString += chunk;
-            });
-            res.on('end', () => {
-                if (dataString) {
-                    let quoteBody = dataString;
-                    let quote = {};
+exports.handler = async () => {
+  let dataString = "";
 
-                    if (dataString.indexOf('(') > -1) {
-                        const quoteBodyBits = quoteBody.split('(');
-                        const [quoteText, quoteAuthor] = quoteBodyBits;
-
-                        quote = {
-                            quoteText,
-                            quoteAuthor: quoteAuthor.substring(0, quoteAuthor.length - 1),
-                        };
-                    } else {
-                        quote = {
-                            quoteText: quoteBody,
-                            quoteAuthor: 'Unknown',
-                        };
-                    }
-
-                    return resolve({
-                        statusCode: 200,
-                        body: JSON.stringify(quote)
-                    });
-                }
-
-                return resolve({
-                    statusCode: 200,
-                    body: "Quote service not available"
-                });
-            });
+  const response = await new Promise((resolve, reject) => {
+    const req = https.get(
+      "https://api.forismatic.com/api/1.0/?method=getQuote&format=text&lang=en",
+      (res) => {
+        res.on("data", (chunk) => {
+          dataString += chunk;
         });
+        res.on("end", () => {
+          if (dataString) {
+            const quoteBody = dataString;
+            let quote = {};
 
-        req.on('error', (e) => {
-            reject({
-                statusCode: 500,
-                body: 'Something went wrong!'
+            if (dataString.indexOf("(") > -1) {
+              const quoteBodyBits = quoteBody.split("(");
+              const [quoteText, quoteAuthor] = quoteBodyBits;
+
+              quote = {
+                quoteText,
+                quoteAuthor: quoteAuthor.substring(0, quoteAuthor.length - 1),
+              };
+            } else {
+              quote = {
+                quoteText: quoteBody,
+                quoteAuthor: "Unknown",
+              };
+            }
+
+            return resolve({
+              statusCode: 200,
+              body: JSON.stringify(quote),
             });
+          }
+
+          return resolve({
+            statusCode: 200,
+            body: "Quote service not available",
+          });
         });
+      }
+    );
+
+    req.on("error", () => {
+      reject({
+        // eslint-disable-line prefer-promise-reject-errors
+        statusCode: 500,
+        body: "Something went wrong!",
+      });
     });
+  });
 
-    return response;
+  return response;
 };
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
-const express = require('express');
+const express = require("express");
+
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', (req, res) => {
-  res.render('index', { title: 'Pomodoro App' });
+router.get("/", (req, res) => {
+  res.render("index", { title: "Pomodoro App" });
 });
 
 module.exports = router;
