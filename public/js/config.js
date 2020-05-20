3204d63152ae7c35254b7e86cde37c3032aefa44
Veronica MIHAI <mihaiveronica@ymail.com>
2020-05-20 07:54:12 +0300

    config file with AWS Lambda functions API Gateway endpoints

---
+++ public/js/config.js
@@ -0,0 +1,4 @@
+export default {
+    GET_WEATHER: 'https://rfxbk8gcti.execute-api.eu-west-2.amazonaws.com/default/getOpenWeather',
+    GET_QUOTE: 'https://1k2r38b4w2.execute-api.eu-west-2.amazonaws.com/default/getForismaticQuote'
+};
