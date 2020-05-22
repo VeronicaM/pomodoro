49c0d0fb150d5a4eb414c7cd5c1bc1baaf2ce75c
Veronica MIHAI <mihaiveronica@ymail.com>
2020-05-22 08:15:35 +0300

    extract quote widget code into separate MVC logic

---
+++ public/js/components/Quote/quote.controller.js
@@ -0,0 +1,90 @@
+// services
+import { buildURL } from '../../services/endpoint.service';
+import { setSource, updateText } from '../../common/utils';
+
+export default class QuoteController {
+    constructor() {
+        // elements
+        this.label = $(".text");
+        this.author = $(".author");
+        this.closeBtn = $("#closeQuote");
+        this.twitterIcon = $("#twitter");
+        this.container = $(".quote");
+        this.containerToggle = $(".quoteTrigger");
+        this.shareContainer = $(".shareQ");
+
+        this.initView();
+    }
+
+    initView() {
+        const onGetQuote = (quote) => {
+            this.displayQuote(quote);
+            this.addEventListeners();
+        };
+
+        this.getQuote().then(onGetQuote.bind(this));
+    }
+
+    getQuote() {
+        const URL = buildURL("GET_QUOTE");
+
+        return new Promise((resolve) => {
+            $.post(URL).done((quote) => {
+                const result = JSON.parse(quote);
+                console.log(result);
+                resolve({
+                    author: result.quoteAuthor || 'Unknown',
+                    text: result.quoteText
+                });
+            });
+        });
+    }
+
+    displayQuote({ text, author }) {
+        updateText(this.label, `" ${text} "`);
+        updateText(this.author, `" ${author} "`);
+    }
+
+    addEventListeners() {
+        // click events
+        this.containerToggle.on("click", this.onContainerToggle.bind(this));
+        this.closeBtn.on("click", this.onCloseContainer.bind(this));
+        this.twitterIcon.click(this.onTweet);
+
+        // hover events
+        this.container.hover(this.onHoverIn.bind(this));
+        this.container.mouseleave(this.onHoverOut.bind(this));
+    }
+
+    onCloseContainer() {
+        this.container.hide();
+    }
+
+    onContainerToggle(e) {
+        if (e.target === this.containerToggle[0]) {
+            this.container.toggle();
+        }
+    }
+
+    onHoverIn() {
+        this.shareContainer.addClass("animateQuote");
+    }
+
+    onHoverOut() {
+        this.shareContainer.removeClass("animateQuote");
+    }
+
+    onTweet() {
+        let textToTweet = `${$(".text").text()} by ${$(".author").text()}`;
+        if (textToTweet.length >= 140) {
+            textToTweet = `${textToTweet.substring(0, 70)}... by ${$(
+                ".author"
+            ).text()}`;
+        }
+        const twtLink = `http://twitter.com/home?status=${encodeURIComponent(
+            textToTweet
+        )}`;
+        window.open(twtLink, "_blank");
+    }
+}
+
