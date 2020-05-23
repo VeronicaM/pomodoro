f038a00d2f8217903e3b6dd4f7bdc3b00687bdfd
Veronica MIHAI <mihaiveronica@ymail.com>
2020-05-23 10:27:18 +0300

    cleanup main controller and move injectFavicon in app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,25 @@
 // CSS
 require("../css/app.scss");
-
 import MainController from './main.controller';
+
+const favIco = require("../favicon.png");
+
+
+function injectFavIcon() {
+  const favIco = require("../favicon.png");
+  const link = $("<link></link>");
+  link[0].type = "image/x-icon";
+  link[0].rel = "shortcut icon";
+  link[0].href = favIco;
+  $("head")[0].append(link[0]);
+}
 
 $(() => {
   // request permission on page load
   window.onload = function onLoad() {
     $(document).ready(() => {
+      injectFavIcon();
+
       const main = new MainController();
     });
   };
