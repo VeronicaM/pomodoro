fecc02275d03aa62051011837d3b6e36a5ebd141
Veronica Mihai <mihaiveronica@ymail.com>
2020-05-24 09:57:01 +0300

    Hotfix/fix bugs after eslint pr (#6)
    * extract weather widget code into separate MVC logic
    
    * extract quote widget code into separate MVC logic
    
    * remove view files as moved into separate components
    
    * add endpoint service
    
    * remove not needed file as logic separated into components
    
    * - extract quote widget into separate component from main view
    - update views paths
    
    * common utils
    
    * refactor css to use BEM notation for weather widget
    
    * refactor css to use BEM notation for quote widget
    
    * fix styling for weather widget close icon
    
    * extract all logic from app.js into separate MVC components and use main controller to render app
    
    * extract tasks widget code into separate MVC logic
    
    * refactor css to use BEM notation for tasks widget
    
    * don't allow empty tasks to be added in ToDos
    
    * - extract settings widget code into separate MVC logic with its sub sections for links and timer settings
    - refactor CSS to use BEM notation
    
    * Timer Settings section MVC component
    
    * extract css animations into separate file
    
    * extract links settings widget section  code into separate MVC logic
    
    * add click outside functionality to close settings widget when user clicks outside of it
    
    * refactor css to use BEM notation and js-element-id naming convensions in settings widget
    
    * cleanup main controller and move injectFavicon in app.js
    
    * fix initial settings rendering
    
    * refactor clock Widget into separate MVC component
    
    * hotfix: default links
    styling: remove spaces and quotes for author name
    
    * hotfix clockWidget sessions logic

---
+++ public/js/components/SettingsWidget/TimerSection/timer.controller.js
@@ -0,0 +1,91 @@
+
+// helpers
+import { swap } from '../../../common/utils';
+
+const sounds = {
+    Jubilation: "jubilation.mp3",
+    "Give me your attention": "attention-seeker.mp3",
+    "Good Morning": "good-morning.mp3",
+    Chime: "long-chime-sound.mp3",
+    "Hey you": "obey.mp3",
+    Solemn: "solemn.mp3",
+    "Tic Tac": "oringz-w435.mp3",
+};
+
+export default class TimerController {
+    constructor() {
+        // settings options
+        this.pomodoroSessionTime = $("#js-countdown");
+        this.breakTime = $("#js-break-minutes");
+        this.breakSound = $("#js-break-sound");
+        this.workSound = $("#js-work-sound");
+        this.settingsOption = $(".settings-widget-timer__option");
+
+        // state
+        this.soundNames = swap(sounds);
+
+        this.initView();
+    }
+
+    initView() {
+        this.settings = TimerController.getSettings();
+
+        this.renderSettings();
+
+        // add event listeners
+        this.settingsOption.change(this.onSettingsChange.bind(this));
+    }
+
+    updateState(newState) {
+        this.state = { ...this.state, newState };
+    }
+
+    renderSettings() {
+        this.pomodoroSessionTime.val(this.settings['countdown']);
+        this.breakTime.val(this.settings['break-minutes']);
+        this.breakSound.val(this.soundNames[this.settings['break-sound']]);
+        this.workSound.val(this.soundNames[this.settings['work-sound']]);
+    }
+
+    static getSettings() {
+        const storedSettings = JSON.parse(localStorage.getItem("pomodoroSettings")) || {};
+
+        const settings = {
+            'break-minutes': storedSettings['break-minutes'] || "5",
+            countdown: storedSettings['countdown'] || "25",
+            'break-sound': storedSettings['break-sound'] || "good-morning.mp3",
+            'work-sound': storedSettings['work-sound'] || "attention-seeker.mp3",
+            minutes: storedSettings["minutes"] || storedSettings['countdown'] || "25",
+            seconds: storedSettings['seconds'] || 59,
+            running: storedSettings['running'] || false,
+            isBreak: storedSettings['isBreak'] || false
+        };
+
+        localStorage.setItem("pomodoroSettings", JSON.stringify(settings));
+
+        return settings;
+    }
+
+    static updateSettings(option) {
+        const settings = JSON.parse(localStorage.getItem("pomodoroSettings") || {});
+        const updatedSettings = Object.assign({}, settings, option);
+        localStorage.setItem("pomodoroSettings", JSON.stringify(updatedSettings));
+        this.settings = updatedSettings;
+    }
+
+    onSettingsChange(e) {
+        const getCleanId = (id) => id.replace('js-', '');
+
+        const newOption = {};
+        const key = getCleanId(e.currentTarget.getAttribute('id'));
+        let value = "";
+        if (key.indexOf("sound") > 0) {
+            value = sounds[e.currentTarget.value];
+        } else {
+            value = e.currentTarget.value;
+        }
+
+        newOption[key] = value;
+        TimerController.updateSettings(newOption);
+    }
+}
---
+++ public/js/components/TasksWidget/tasks.controller.js
@@ -0,0 +1,163 @@
+import Task from './Task';
+
+export default class TasksController {
+  constructor() {
+    // elements
+    this.widget = $(".tasks-widget");
+    this.widgetToggle = $(".tasks-widget__trigger");
+    this.container = $(".tasks-widget__tasks");
+    this.deleteIconSelector = "tasks-widget__delete";
+    this.checkboxSelector = "tasks-widget__checkbox";
+    this.completeTaskClassName = "strike-text";
+    this.taskInput = $("#taskName");
+
+    // properties
+    this.tasks = [];
+
+    this.initView();
+
+    // add event listeners
+    this.widgetToggle.on("click", this.onContainerToggle.bind(this));
+    this.taskInput.keypress(this.onTypeTask.bind(this));
+  }
+
+  initView() {
+    this.tasks = this.getTasks();
+    this.print();
+  }
+
+  saveTask(task) {
+    const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+    tasks.push(task);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+    this.tasks = tasks;
+  }
+
+  removeTask(task) {
+    const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+    const filteredTasks = tasks.filter((t) => t.description !== task.description);
+    localStorage.setItem("tasks", JSON.stringify(filteredTasks));
+    this.tasks = tasks;
+  }
+
+  completeTask(task) {
+    const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+    const index = tasks.findIndex(
+      (taskItem) => taskItem.description === task.description
+    );
+
+    tasks[index] = task;
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+    this.tasks = tasks;
+  }
+
+  getTasks() {
+    const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+    return tasks;
+  }
+
+  generateTaskTemplate(task) {
+    const appendDelete = `<span class='${this.deleteIconSelector}' data-taskValue='${JSON.stringify(
+      task
+    )}'> x </span>`;
+    const className = task.completeTask ? this.completeTaskClassName : "";
+    const content = `<label class='${className}'><input taskValue='${JSON.stringify(
+      task
+    )}' class='${this.checkboxSelector}' type ='checkbox' value ='${task.description}'> ${
+      task.description
+      }${appendDelete}</label>`;
+
+    return content;
+  }
+
+  printTask(task) {
+    const taskTemplate = this.generateTaskTemplate(task);
+    this.container.append(taskTemplate);
+    this.addEventListners();
+  }
+
+  addEventListners() {
+    $(`.${this.deleteIconSelector}`).on("click", this.onDeleteTask.bind(this));
+    $(`.${this.checkboxSelector}`).change(this.onToggleTaskComplete.bind(this));
+  }
+
+  generateTasksTemplate() {
+    let content = "";
+    let appendDelete = "";
+    let className = "";
+    let checked = "";
+    for (let i = 0; i < this.tasks.length; i += 1) {
+      appendDelete = `<span class='${this.deleteIconSelector}' data-taskValue='${JSON.stringify(
+        this.tasks[i]
+      )}'> x </span>`;
+      if (this.tasks[i].completed) {
+        className = this.completeTaskClassName;
+        checked = "checked";
+      }
+      content += `<label class='${className}'><input taskValue='${JSON.stringify(
+        this.tasks[i]
+      )}' class='${this.checkboxSelector}' type ='checkbox' ${checked} value ='${
+        this.tasks[i].description
+        }'> ${this.tasks[i].description}${appendDelete}</label>`;
+      className = "";
+      checked = "";
+    }
+
+    return content;
+  }
+
+  print() {
+    const tasksTemplate = this.generateTasksTemplate();
+    this.container.empty();
+    this.container.append(tasksTemplate);
+
+    this.addEventListners();
+  }
+
+  onDeleteTask(e) {
+    const attributeContent = e.currentTarget.getAttribute("data-taskValue");
+    const object = JSON.parse(attributeContent);
+
+    this.removeTask(object);
+
+    this.initView();
+  }
+
+  onToggleTaskComplete(e) {
+    const attributeContent = e.currentTarget.getAttribute("taskValue");
+    const object = JSON.parse(attributeContent);
+
+    if (e.currentTarget.checked) {
+      e.currentTarget.parentElement.className = this.completeTaskClassName;
+      object.completed = true;
+    } else {
+      e.currentTarget.parentElement.className = "";
+      object.completed = false;
+    }
+
+    this.completeTask(object);
+  }
+
+  onContainerToggle(e) {
+    if (e.target === this.widgetToggle[0]) {
+      this.widget.toggle();
+    }
+  }
+
+  onTypeTask(e) {
+    const key = e.which;
+    if (key === 13 && e.currentTarget.value) {
+      // the enter key code
+      const newTask = new Task({
+        description: e.currentTarget.value,
+        completed: false,
+      });
+
+      this.saveTask(newTask);
+      this.printTask(newTask);
+
+      // reset current value
+      e.currentTarget.value = "";
+    }
+  }
+}
